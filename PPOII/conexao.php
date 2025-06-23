db855ddc94accb85c09495c1798e8472919ff70e
NatanMB132 <131016965+NatanMB132@users.noreply.github.com>
2025-06-23 18:37:23 +0000

    aaa

--- PPOII/conexao.php
+++ PPOII/conexao.php
@@ -4,13 +4,11 @@ $usuario = "root";
 $senha = "";
 $banco = "meu_site";
 
-
 $conn = new mysqli($servidor, $usuario, $senha, $banco);
-
 
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
 
-//echo "Conectado com sucesso!";
+echo "Conectado com sucesso!";
 ?>
